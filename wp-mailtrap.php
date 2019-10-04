f75691c0acb060a271291ee687ec4edb0f218110
Mehrshad Darzi <mehrshad198@gmail.com>
2019-10-04 10:56:24 +0330

    first commit

---
+++ wp-mailtrap.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Mailtrap.io Test Email
+Plugin URI: https://mailtrap.io/
+Description: Mailtrap.io Test Email
+Author: Mehrshad Darzi
+Version: 500.0
+Author URI: https://realwp.net
+*/
+function mailtrap( $phpmailer ) {
+	$phpmailer->isSMTP();
+	$phpmailer->Host     = 'smtp.mailtrap.io';
+	$phpmailer->SMTPAuth = true;
+	$phpmailer->Port     = 2525;
+	$phpmailer->Username = MAILTRAP_USERNAME;
+	$phpmailer->Password = MAILTRAP_PASSWORD;
+}
+
+if ( in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) and defined('MAILTRAP_USERNAME') and defined('MAILTRAP_PASSWORD')) { ) {
+	add_action( 'phpmailer_init', 'mailtrap' );
+}
